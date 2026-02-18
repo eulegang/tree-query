11dfab66f3e8645b92619faa9eea922855c5ae95
eulegang <eulegang@eulegang.dev>
2026-02-17 22:07:38 -0600

    fixing type detection and adding override

--- src/tree.cpp
+++ src/tree.cpp
@@ -9,11 +9,19 @@ void action_tree(state &state, Cli &cli) {
   mfile file(cli.arg);
 
   std::filesystem::path path = {cli.arg};
-  if (const auto ty = state.map.resolve(path); ty) {
-    std::cout << *ty << std::endl;
-    ts::lang lang = state.reg.load(cli.type);
-    ts::parser parser{lang};
-    ts::tree tree = parser.parse(file);
-    std::cout << (std::string)tree << std::endl;
+
+  std::string grammar;
+  if (cli.type != "") {
+    grammar = cli.type;
+  } else if (const auto ty = state.map.resolve(path); ty) {
+    grammar = *ty;
+  } else {
+    std::cerr << "grammar not specificed" << std::endl;
+    exit(1);
   };
+
+  ts::lang lang = state.reg.load(grammar);
+  ts::parser parser{lang};
+  ts::tree tree = parser.parse(file);
+  std::cout << (std::string)tree << std::endl;
 }
